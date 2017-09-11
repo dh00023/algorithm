51568f29aa62b5c87643c2f833c048d4e71ba091
dahye Jeong <wjdekgp1750@naver.com>
2017-09-11 13:45:19 +0900

    star17

--- inputoutput/star.cpp
+++ inputoutput/star.cpp
@@ -2,7 +2,7 @@
 #include <string>
 using namespace std;
 
-//    첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+// 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
 void star1(){
     int N;
     scanf("%d",&N);
@@ -13,7 +13,7 @@ void star1(){
         printf("\n");
     }
 }
-//첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제 하지만, 오른쪽을 기준으로 정렬한 별 (예제 참고)을 출력하시오.
+// 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제 하지만, 오른쪽을 기준으로 정렬한 별 (예제 참고)을 출력하시오.
 void star2(){
     int n;
     scanf("%d",&n);
@@ -26,7 +26,7 @@ void star2(){
         printf("\n");
     }
 }
-//첫째 줄에는 별 N개, 둘째 줄에는 별 N-1개, ..., N번째 줄에는 별 1개를 찍는 문제
+// 첫째 줄에는 별 N개, 둘째 줄에는 별 N-1개, ..., N번째 줄에는 별 1개를 찍는 문제
 void star3(){
     int N;
     scanf("%d",&N);
@@ -37,7 +37,7 @@ void star3(){
         printf("\n");
     }
 }
-//첫째 줄에는 별 N개, 둘째 줄에는 별 N-1개, ..., N번째 줄에는 별 1개를 찍는 문제 하지만, 오른쪽을 기준으로 정렬한 별 (예제 참고)을 출력하시오.
+// 첫째 줄에는 별 N개, 둘째 줄에는 별 N-1개, ..., N번째 줄에는 별 1개를 찍는 문제 하지만, 오른쪽을 기준으로 정렬한 별 (예제 참고)을 출력하시오.
 void star4(){
     int n;
     scanf("%d",&n);
@@ -50,7 +50,7 @@ void star4(){
         printf("\n");
     }
 }
-//첫째 줄에는 별 1개, 둘째 줄에는 별 3개, ..., N번째 줄에는 별 2*N-1개를 찍는 문제 별은 가운데를 기준으로 대칭이어야 한다.
+// 첫째 줄에는 별 1개, 둘째 줄에는 별 3개, ..., N번째 줄에는 별 2*N-1개를 찍는 문제 별은 가운데를 기준으로 대칭이어야 한다.
 void star5(){
     int n;
     scanf("%d",&n);
@@ -62,9 +62,9 @@ void star5(){
         printf("\n");
     }
 }
-////첫째 줄에는 별 1개, 둘째 줄에는 별 3개, ..., N번째 줄에는 별 2*N-1개를 찍는 문제 별은 가운데를 기준으로 대칭이어야 한다.
-//다이아온드모양
-void star6(){
+// 첫째 줄에는 별 1개, 둘째 줄에는 별 3개, ..., N번째 줄에는 별 2*N-1개를 찍는 문제 별은 가운데를 기준으로 대칭이어야 한다.
+// 다이아온드모양
+void star7(){
     int n;
     scanf("%d",&n);
     for(int i=1;i<=n;i++){
@@ -82,7 +82,7 @@ void star6(){
         printf("\n");
     }
 }
-void star7(){
+void star8(){
     int n;
     scanf("%d",&n);
     for(int i=n-1;i>=1;i--){
@@ -100,7 +100,7 @@ void star7(){
         printf("\n");
     }
 }
-void star8(){
+void star9(){
     int n;
     scanf("%d",&n);
     for(int i=n;i>1;i--){
@@ -119,7 +119,7 @@ void star8(){
     }
     
 }
-void star9(){
+void star12(){
     int n;
     scanf("%d",&n);
     for(int i=1;i<=n;i++){
@@ -137,7 +137,7 @@ void star9(){
         printf("\n");
     }
 }
-void star10(){
+void star16(){
     int n;
     scanf("%d",&n);
     for(int i=1;i<=n;i++){
@@ -148,21 +148,26 @@ void star10(){
         printf("\n");
     }
 }
-void star11(){
+void star17(){
     int n;
     scanf("%d",&n);
     for(int i=1;i<=n;i++){
-        for(int j=0;j<=2*i-1;j++){
-            printf("%d",j);
+        for(int j=1;j<=n-i;j++){
+            printf(" ");
+        }
+        for(int t=1;t<=2*i-1;t++){
+            if(t%2==0){
+                printf(" ");
+            }else if(t%2==1){
+                printf("*");
+            }
         }
         printf("\n");
     }
 }
 
-
 int main(int argc, const char * argv[]) {
     // insert code here...
-    //star11();a
-    
+    //star11();
     return 0;
 }
